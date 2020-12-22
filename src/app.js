14b3721e601b201908632dc442e14dc49741049d
Eric Dang <edang830@gmail.com>
2020-12-21 20:38:18 -0800

    index and app into src and public dirs

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('index.html');
 });
 
 const PORT = process.env.PORT || 5000;

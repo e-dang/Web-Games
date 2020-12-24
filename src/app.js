b18aa8c8ef0900c43ff062113fd0b21583958c5b
Eric Dang <edang830@gmail.com>
2020-12-24 14:09:27 -0800

    Split app and server

--- src/app.js
+++ src/app.js
@@ -9,6 +9,4 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;

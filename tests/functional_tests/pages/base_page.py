35166e23282db20496fac0bad2e044c59fb29fcf
Eric Dang <edang830@gmail.com>
2020-12-24 16:00:45 -0800

    Get FT to fail on has_board()

--- tests/functional_tests/pages/base_page.py
+++ tests/functional_tests/pages/base_page.py
@@ -16,14 +16,17 @@ class BasePage:
         return self.driver.title == f'Web Games - {title}'
 
     def has_correct_header(self, header):
-        return self.driver.wait_to_find_by_id('currentGame').text == header
+        return self.driver.find_element_by_id('currentGame').text == header
 
     def has_navigation_bar(self):
-        return self.driver.find_element_by_id('navBar').is_displayed()
+        return self._get_navbar().is_displayed()
 
     def has_board(self):
         return self.driver.find_element_by_id('gameBoard').is_displayed()
 
     def can_select_games(self):
-        element = self.driver.find_element_by_id('gameSelection')
+        element = self._get_navbar().find_element_by_id('gameSelection')
         return element.is_displayed() and element.is_enabled()
+
+    def _get_navbar(self):
+        return self.driver.find_element_by_id('navBar')

cf75679609d431244888fe6af82501b33e5e7bb1
Eric Dang <edang830@gmail.com>
2021-01-05 00:11:49 -0800

    Implement change event handler for node inputs

--- src/sudoku/sudoku_page.js
+++ src/sudoku/sudoku_page.js
@@ -6,8 +6,24 @@ class SudokuPage {
     constructor() {
         this.board = new SudokuBoard(DIMENSIONS);
 
-        this.board.draw();
+        this.board.draw((node) => this._customizeNode(node));
         this.board.reset();
+    }
+    _customizeNode(node) {
+        node.addInputEventListener('change', () => this._handleInputChangeEvent(node));
+    }
+
+    _handleInputChangeEvent(node) {
+        if (node.userValueIsCorrect()) {
+            if (this.board.isComplete()) {
+                this._handleSudokuComplete();
+            }
+        }
+    }
+
+    _handleSudokuComplete() {
+        document.getElementById('gameOverTitle').innerText = 'Congratulations, You Won!';
+        $('#gameOverModal').modal();
     }
 }
 
--- tests/unit/sudoku/sudoku_page.test.js
+++ tests/unit/sudoku/sudoku_page.test.js
@@ -1,13 +1,22 @@
 const SudokuBoard = require('../../../src/sudoku/sudoku_board');
+const SudokuGameNode = require('../../../src/sudoku/sudoku_game_node');
 const SudokuPage = require('../../../src/sudoku/sudoku_page');
+const {loadHTML, clearHTML} = require('../utils');
 
 jest.mock('../../../src/sudoku/sudoku_board');
+jest.mock('../../../src/sudoku/sudoku_game_node');
 
 describe('test SudokuPage', () => {
     let page;
 
-    beforeEach(() => {
+    beforeEach(async (done) => {
+        await loadHTML('/sudoku');
         page = new SudokuPage();
+        done();
+    });
+
+    afterEach(() => {
+        clearHTML();
     });
 
     test('constructor sets board prop to an instance of SudokuBoard', () => {
@@ -20,5 +29,25 @@ describe('test SudokuPage', () => {
 
     test('constructor calls reset method on board property', () => {
         expect(page.board.reset).toHaveBeenCalledTimes(1);
+    });
+
+    test('_handleInputChangeEvent calls _handleSudokuComplete when node.userValueIsCorrect and board.isComplete returns true', () => {
+        const node = new SudokuGameNode();
+        node.userValueIsCorrect.mockReturnValueOnce(true);
+        page.board.isComplete.mockReturnValueOnce(true);
+        page._handleSudokuComplete = jest.fn();
+
+        page._handleInputChangeEvent(node);
+
+        expect(page._handleSudokuComplete).toHaveBeenCalledTimes(1);
+    });
+
+    test('_handleSudokuComplete displays gameOverModal', async (done) => {
+        $('#gameOverModal').on('shown.bs.modal', (event) => {
+            expect(event.target).toHaveClass('show');
+            done();
+        });
+
+        page._handleSudokuComplete();
     });
 });

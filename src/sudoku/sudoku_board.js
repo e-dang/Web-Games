3363eb23e40d9d07256ea4e4ef8f6f6fd3750af2
Eric Dang <edang830@gmail.com>
2021-01-05 17:16:33 -0800

    Implment sudoku game generation algorithm

--- src/sudoku/sudoku_board.js
+++ src/sudoku/sudoku_board.js
@@ -1,20 +1,49 @@
 const Board = require('../core/board');
 const SudokuGameNode = require('./sudoku_game_node');
+const SudokuSolver = require('./sudoku_solver');
 
 class SudokuBoard extends Board {
     constructor(dimensions) {
         super(dimensions, SudokuGameNode);
+        this.solver = new SudokuSolver();
     }
 
     reset() {
         this.clear();
-        const node = this.getNode(0, 0);
-        node.trueValue = 1;
-        node.renderTrueValue();
+        this.solver.solve(this).next();
+        this._selectInputNodes(5);
+        this.nodes.forEach((node) => node.renderTrueValue());
     }
 
     isComplete() {
         return this.nodes.reduce((accum, node) => accum + node.userValueIsCorrect(), 0) == this.nodes.length;
+    }
+
+    addNodeInputEventListeners(type, fn) {
+        this.nodes.forEach((node) => node.addInputEventListener(type, () => fn(node)));
+    }
+
+    _selectInputNodes(attempts) {
+        while (attempts > 0) {
+            const node = this._getRandGivenNode();
+
+            node.setAsInputNode();
+            if (this.solver.calcNumSolutions(this) > 1) {
+                attempts--;
+                node.setAsGivenNode();
+            }
+        }
+    }
+
+    _getRandGivenNode() {
+        while (true) {
+            const row = Math.floor(Math.random() * this.dims);
+            const col = Math.floor(Math.random() * this.dims);
+            const node = this.getNode(row, col);
+            if (node.isGivenNode()) {
+                return node;
+            }
+        }
     }
 }
 
--- tests/unit/sudoku/sudoku_board.test.js
+++ tests/unit/sudoku/sudoku_board.test.js
@@ -1,7 +1,9 @@
 const SudokuBoard = require('../../../src/sudoku/sudoku_board');
 const SudokuGameNode = require('../../../src/sudoku/sudoku_game_node');
+const SudokuSolver = require('../../../src/sudoku/sudoku_solver');
 
 jest.mock('../../../src/sudoku/sudoku_game_node');
+jest.mock('../../../src/sudoku/sudoku_solver');
 
 describe('Test SudokuBoard', () => {
     let board;
@@ -14,6 +16,7 @@ describe('Test SudokuBoard', () => {
 
     afterEach(() => {
         SudokuGameNode.mockClear();
+        SudokuSolver.mockClear();
     });
 
     test('constructor calls Board constructor with dimensions param and SudokuGameNode', () => {
@@ -27,19 +30,57 @@ describe('Test SudokuBoard', () => {
         expect(mock).toHaveBeenCalledWith(dims, SudokuGameNode);
     });
 
+    test('constructor creates a new SudokuSolver object and sets it to solver prop', () => {
+        expect(board.solver).toBeInstanceOf(SudokuSolver);
+    });
+
     test('reset calls clear method', () => {
-        board.getNode = jest.fn().mockReturnValueOnce(new SudokuGameNode());
         board.clear = jest.fn();
+        board._selectInputNodes = jest.fn();
+        board.solver.solve.mockReturnValueOnce({next: jest.fn()});
 
         board.reset();
 
         expect(board.clear).toHaveBeenCalledTimes(1);
     });
 
-    test('isComplete returns true when all nodes have correct user values', () => {
+    test('reset calls solve on solver prop', () => {
+        board.clear = jest.fn();
+        board._selectInputNodes = jest.fn();
+        board.solver.solve.mockReturnValueOnce({next: jest.fn()});
+
+        board.reset();
+
+        expect(board.solver.solve).toHaveBeenCalledWith(board);
+    });
+
+    test('reset calls _selectInputNodes', () => {
+        board.clear = jest.fn();
+        board._selectInputNodes = jest.fn();
+        board.solver.solve.mockReturnValueOnce({next: jest.fn()});
+
+        board.reset();
+
+        expect(board._selectInputNodes).toHaveBeenCalledTimes(1);
+    });
+
+    test('reset calls renderTrueValue on each node', () => {
+        board.clear = jest.fn();
+        board._selectInputNodes = jest.fn();
+        board.nodes = [new SudokuGameNode(), new SudokuGameNode(), new SudokuGameNode()];
+        board.solver.solve.mockReturnValueOnce({next: jest.fn()});
+
+        board.reset();
+
+        board.nodes.forEach((node) => {
+            expect(node.renderTrueValue).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    test('isComplete returns true when all input nodes have correct user values', () => {
         for (let i = 0; i < dims; i++) {
             const node = new SudokuGameNode();
-            node.userValueIsCorrect.mockReturnValueOnce(true);
+            node.userValueIsCorrect.mockReturnValue(true);
             board.nodes.push(node);
         }
 
@@ -48,13 +89,13 @@ describe('Test SudokuBoard', () => {
         expect(retVal).toBe(true);
     });
 
-    test('isComplete returns false when at least one node doesnt have correct user value', () => {
+    test('isComplete returns false when at least one input node doesnt have correct user value', () => {
         for (let i = 0; i < dims; i++) {
             const node = new SudokuGameNode();
             if (i != 0) {
-                node.userValueIsCorrect.mockReturnValueOnce(true);
+                node.userValueIsCorrect.mockReturnValue(true);
             } else {
-                node.userValueIsCorrect.mockReturnValueOnce(false);
+                node.userValueIsCorrect.mockReturnValue(false);
             }
             board.nodes.push(node);
         }
@@ -62,5 +103,40 @@ describe('Test SudokuBoard', () => {
         const retVal = board.isComplete();
 
         expect(retVal).toBe(false);
+    });
+
+    test('_getRandGivenNode returns a given node', () => {
+        const inputNode = new SudokuGameNode();
+        const givenNode = new SudokuGameNode();
+        inputNode.isGivenNode.mockReturnValueOnce(false);
+        givenNode.isGivenNode.mockReturnValueOnce(true);
+        board.getNode = jest.fn().mockReturnValueOnce(inputNode).mockReturnValueOnce(givenNode);
+
+        const retVal = board._getRandGivenNode();
+
+        expect(retVal).toBe(givenNode);
+    });
+
+    test('_selectInputNodes calls setAsInputNode at least attempts number of times', () => {
+        const attempts = 2;
+        const node = new SudokuGameNode();
+        board._getRandGivenNode = jest.fn(() => node);
+        board.solver.calcNumSolutions.mockReturnValue(2);
+
+        board._selectInputNodes(attempts);
+
+        expect(node.setAsInputNode).toHaveBeenCalledTimes(2);
+    });
+
+    test('addNodeInputEventListeners calls addInputEventListener on each node', () => {
+        board.nodes = [new SudokuGameNode(), new SudokuGameNode(), new SudokuGameNode()];
+        const type = 'change';
+        const fn = jest.fn();
+
+        board.addNodeInputEventListeners(type, fn);
+
+        board.nodes.forEach((node) => {
+            expect(node.addInputEventListener).toHaveBeenCalled();
+        });
     });
 });

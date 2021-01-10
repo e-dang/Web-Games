b0a47bfa1be23120a94d6cee8e11fb0bd5ca5e60
Eric Dang <edang830@gmail.com>
2021-01-10 12:40:32 -0800

    Implement Page that add diffuclty and reset btns

--- src/tic_tac_toe/tic_tac_toe_page.js
+++ src/tic_tac_toe/tic_tac_toe_page.js
@@ -1,10 +1,19 @@
+const Page = require('../core/page');
 const {TicTacToeBoard} = require('./tic_tac_toe_board');
 
-class TicTacToePage {
+class TicTacToePage extends Page {
     constructor() {
+        super();
         this.board = new TicTacToeBoard();
+        this.difficultyMap = {
+            easy: (callback) => callback(),
+            moderate: (callback) => callback(),
+            hard: (callback) => callback(),
+        };
 
+        this.addEventHandlers();
         this.board.draw();
+        this._setDefaultDifficulty();
     }
 }
 

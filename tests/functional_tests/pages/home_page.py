d1b2c412d8798ef7870ab30cccd81e11c95831e4
Eric Dang <edang830@gmail.com>
2021-01-12 20:51:23 -0800

    Add FT for navigating using cards on homepage

--- tests/functional_tests/pages/home_page.py
+++ tests/functional_tests/pages/home_page.py
@@ -7,3 +7,14 @@ class HomePage(BasePage):
 
     def has_correct_header(self):
         return super().has_correct_header(None)
+
+    def select_game_using_cards(self, game):
+        id_map = {
+            'snake': 'snakeCard',
+            'sudoku': 'sudokuCard',
+            'tic-tac-toe': 'ticTacToeCard'
+        }
+        self.driver.find_element_by_id(id_map[game]).click()
+
+    def click_home_page(self):
+        self.driver.find_element_by_id('navBarHeader').click()
--- tests/functional_tests/test_navigation.py
+++ tests/functional_tests/test_navigation.py
@@ -9,7 +9,7 @@ class TestNavigation:
     def url(self):
         return 'http://localhost:5000/'
 
-    def test_can_navigate_to_different_page_using_nav_bar(self, url):
+    def test_user_can_navigate_to_different_page_using_nav_bar(self, url):
 
         # the user goes to the page
         self.driver.get(url)
@@ -26,3 +26,18 @@ class TestNavigation:
         for game in games:
             page.select_game(game)
             assert game in self.driver.current_url
+
+    def test_user_navigate_to_different_page_using_cards(self, url):
+        # the user goes to the page
+        self.driver.get(url)
+        page = HomePage(self.driver)
+
+        # The user then uses the cards to select different games which brings them to the associated pages
+        games = ['snake', 'sudoku', 'tic-tac-toe']
+        for game in games:
+            page.select_game_using_cards(game)
+            assert game in self.driver.current_url
+
+            # the user then goes back to the home page
+            page.click_home_page()
+            assert url == self.driver.current_url

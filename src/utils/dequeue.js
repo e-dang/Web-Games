e929227cbdceb2c4d38a7148d768497fc61e7e7a
Eric Dang <edang830@gmail.com>
2023-10-14 14:22:10 -0700

    Convert utils to esmodule syntax

--- src/utils/dequeue.js
+++ src/utils/dequeue.js
@@ -1,97 +1,95 @@
-class DequeueNode {
-    constructor(data, next = null, prev = null) {
-        this.data = data;
-        this.next = next;
-        this.prev = prev;
-    }
+export class DequeueNode {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
 }
 
-class Dequeue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+export class Dequeue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  pushFront(data) {
+    const node = new DequeueNode(data);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
     }
 
-    pushFront(data) {
-        const node = new DequeueNode(data);
-        if (this.isEmpty()) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            node.next = this.head;
-            this.head.prev = node;
-            this.head = node;
-        }
+    this.length++;
+  }
 
-        this.length++;
+  pushBack(data) {
+    const node = new DequeueNode(data);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
     }
 
-    pushBack(data) {
-        const node = new DequeueNode(data);
-        if (this.isEmpty()) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            this.tail.next = node;
-            node.prev = this.tail;
-            this.tail = node;
-        }
+    this.length++;
+  }
 
-        this.length++;
+  popFront() {
+    if (this.isEmpty()) {
+      return undefined;
     }
 
-    popFront() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-
-        const node = this.head;
-        this.head = node.next;
-        node.next = null;
-        this.length--;
-        if (!this.isEmpty()) {
-            this.head.prev = null;
-        }
-
-        return node.data;
+    const node = this.head;
+    this.head = node.next;
+    node.next = null;
+    this.length--;
+    if (!this.isEmpty()) {
+      this.head.prev = null;
     }
 
-    popBack() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
+    return node.data;
+  }
 
-        const node = this.tail;
-        this.tail = node.prev;
-        node.prev = null;
-        this.length--;
-        if (!this.isEmpty()) {
-            this.tail.next = null;
-        }
-
-        return node.data;
+  popBack() {
+    if (this.isEmpty()) {
+      return undefined;
     }
 
-    peekFront() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-
-        return this.head.data;
+    const node = this.tail;
+    this.tail = node.prev;
+    node.prev = null;
+    this.length--;
+    if (!this.isEmpty()) {
+      this.tail.next = null;
     }
 
-    peekBack() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
+    return node.data;
+  }
 
-        return this.tail.data;
+  peekFront() {
+    if (this.isEmpty()) {
+      return undefined;
     }
 
-    isEmpty() {
-        return this.length === 0;
+    return this.head.data;
+  }
+
+  peekBack() {
+    if (this.isEmpty()) {
+      return undefined;
     }
+
+    return this.tail.data;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
 }
-
-module.exports = {Dequeue, DequeueNode};
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,47 +1,47 @@
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+export function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function selectRandom(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+export function selectRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function shuffle(array) {
-    // Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+export function shuffle(array) {
+  // Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 
-    var currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
-function millisecondsToSeconds(time) {
-    return time / 1000;
+export function millisecondsToSeconds(time) {
+  return time / 1000;
 }
 
-function setSeed(seed) {
-    if (seed) {
-        Math.seedrandom(seed);
-    }
+export function setSeed(seed) {
+  if (seed) {
+    Math.seedrandom(seed);
+  }
 }
 
-module.exports = {
-    sleep,
-    selectRandom,
-    shuffle,
-    millisecondsToSeconds,
-    setSeed,
+export default {
+  sleep,
+  selectRandom,
+  shuffle,
+  millisecondsToSeconds,
+  setSeed,
 };

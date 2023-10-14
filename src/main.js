4e7b298e2a10107f3a1baf3fc265cb5eac222e19
Eric Dang <edang830@gmail.com>
2023-10-14 13:57:18 -0700

    Remove express files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const createPage = require('./core/create_page');
-const {setSeed} = require('./utils/utils');
-
-setSeed(document.currentScript.getAttribute('seed'));
-const gameType = document.currentScript.getAttribute('game-type');
-const page = createPage(gameType);
